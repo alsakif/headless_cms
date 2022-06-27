7757b803e5c5f845d4778bb10ce44117a3d41439
Sakif Abdullah <sakif@Sakifs-MacBook-Air.local>
2022-06-27 07:25:16 +0600

    skill section is updated without using tooltip

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 
 import './Header.scss';
 import { images } from '../../constants';
+import {AppWrap} from '../../wrapper';
 
 const scaleVariants = {
   whileInView: {
@@ -17,7 +18,7 @@ const scaleVariants = {
 
 function Header() {
   return (
-    <div className='app__header app__flex' id='home'>
+    <div className='app__header app__flex' >
       <motion.div
         whileInView={{x: [-100,0], opacity: [0,1]}}
         transition={{duration: 0.5}}
@@ -69,4 +70,4 @@ function Header() {
   )
 }
 
-export default Header;
+export default AppWrap(Header, 'home');
